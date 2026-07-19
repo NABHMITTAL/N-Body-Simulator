af581211b1f5f3cff7e4cf2279f15cb88ab90ea6
Nabh Mittal <nabh.mittalss@gmail.com>
2026-07-19 23:55:21 +0530

    Updated to work with new vector form

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-def main():
-    print("Project TARS initialized successfully.")
+from body import Body
 
-if __name__ == "__main__":
-    main()
+from vector2 import Vector2
+
+sun = Body(
+  "Sun",
+  1.989e30,
+  Vector2(0, 0),
+  Vector2(0, 0),
+  20,
+  "yellow"
+)
+
+earth = Body(
+  "Earth",
+  5.972e24,
+  Vector2(150, 0),
+  Vector2(0, 29.78),
+  8,
+  "blue"
+)
+
+moon = Body(
+  "Moon",
+  7.35e22,
+  Vector2(151, 0),
+  Vector2(0,30.8),
+  3,
+  "gray"
+)
+
+def __str__(self):
+  return (
+    f"Name: {self.name}\n"
+    f"Mass: {self.mass}\n"
+    f"Position: {self.position}\n"
+    f"Velocity: {self.velocity}\n"
+    f"Radius: {self.radius}\n"
+    f"Color: {self.color}"
+  )
